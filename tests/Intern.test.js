aa0d37eef0640340a0874706168cfd58b2a08eab
bechedley <109792989+bechedley@users.noreply.github.com>
2022-11-22 02:44:40 +1100

    Add test data

---
+++ tests/Intern.test.js
@@ -0,0 +1,26 @@
+const Intern = require("../lib/Intern");
+
+describe("Intern", () => {
+  describe("Initialization", () => {
+    it("should create an object with 'name' string, 'id' number, 'email' string and 'school' string", () => {
+      const intern = new Intern("Vanessa", 20, "vanessa@email.com", "Sydney High School");
+
+      // Verify that the new object has the correct properties
+      expect(intern.name).toEqual("Vanessa");
+      expect(intern.id).toEqual(20);
+      expect(intern.email)toEqual("vanessa@email.com");
+      expect(intern.github)toEqual("Sydney High School");
+    });
+  });
+
+  describe("getRole", () => {
+    it("should return 'Intern'", () => {
+      // Create new objects to test with
+      const intern = new intern("Tom", 10, "tom@email.com", "Melbourne High School");
+
+      // Verify that the role was returned as Intern
+      expect(intern.getRole(intern)).toEqual("Intern");
+    });
+
+  });
+});
---
+++ tests/Manager.test.js
@@ -0,0 +1,26 @@
+const Manager = require("../lib/Manager");
+
+describe("Manager", () => {
+  describe("Initialization", () => {
+    it("should create an object with 'name' string, 'id' number, 'email' string and 'office number' number", () => {
+      const manager = new Manager("Michael", 12, "michael@email.com", 90978788);
+
+      // Verify that the new object has the correct properties
+      expect(manager.name).toEqual("Michael");
+      expect(manager.id).toEqual(12);
+      expect(manager.email)toEqual("michael@email.com");
+      expect(manager.github)toEqual(90978788);
+    });
+  });
+
+  describe("getRole", () => {
+    it("should return 'Manager'", () => {
+      // Create new objects to test with
+      const manager = new Manager("Michele", 6, "michele@email.com", 56576879);
+
+      // Verify that the role was returned as Manager
+      expect(manager.getRole(manager)).toEqual("Manager");
+    });
+
+  });
+});
